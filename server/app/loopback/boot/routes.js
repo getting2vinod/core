c2555c18a76c10529985dfdc5bb347d22448adfe
getting2vinod <vinod.nair@relevancelab.com>
2017-05-31 11:02:40 +0530

    Socket IO issues to looked at

--- server/app/loopback/boot/routes.js
+++ server/app/loopback/boot/routes.js
@@ -3,11 +3,11 @@ var logger = require('_pr/logger')(module);
 
 module.exports = function(app) {
 
-   // logger.debug('Setting up application routes');
+    // logger.debug('Setting up application routes');
     var routes = require('../../routes/v1.0/routes.js');
-    console.log(routes);
+   //console.log(routes);
     console.log("**********************************************************");
-   
+
     var routerV1 = app.loopback.Router();
     routes.setRoutes(routerV1);
     app.use(routerV1);
@@ -15,10 +15,11 @@ module.exports = function(app) {
     app.use('/api/v1.0', routerV1);
 
 
-var routerV2 = require('../../routes/v2.0');
+    var routerV2 = require('../../routes/v2.0');
 
- console.log(routerV2);
+    //console.log(routerV2);
     logger.debug('Setting up version 2 routes');
 
     app.use('/api/v2.0', routerV2);
+    logger.debug("done");
 }
